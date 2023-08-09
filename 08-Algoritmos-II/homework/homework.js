245d14471937f1f750179b1071c17c03ff3f4ac2
Joaquin-Riva <rivajoaquin012@gmail.com>
2023-08-09 15:41:53 -0300

    08-Completado

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,17 +6,62 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  //Si nuestro array tiene un solo elemento devolvemos ese array
+  if(array.length <= 1){
+    return array;
+  }
+  let izq = [];
+  let der = [];
+  let aux = [];
+  let pivote = array.pop();
+  
 
+  for (let i = 0; i < array.length; i++) {
+    //Si el numero es menor o igual al pivote pasaria al lado izq
+    if (array[i] <= pivote) {
+      izq.push(array[i]);
+    //En caso de que sea mayor pasaria a nuestro arreglo der
+    } else {
+      der.push(array[i]);
+    }
+  }
+  //Recursividad
+  return aux.concat(quickSort(izq), pivote, quickSort(der));
 }
+
 
 function mergeSort(array) {
   // Implementar el método conocido como mergeSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  //Si el array recibido es menor o igual a 1 devuelvo el array
+  if(array.length <= 1){
+    return array;
+  }
+  //un let para pararnos en el medio del array
+  let medio = Math.floor(array.length / 2);
+  let izqMerge = mergeSort(array.slice(0, medio));
+  let derMerge = mergeSort(array.slice(medio));
+  return merge(mergeSort(izqMerge), mergeSort(derMerge));
 
-  //okey probemos ahora
 }
+
+function merge(izqMerge, derMerge){
+  let arrSort = []
+  //Chequeamos si los arrays no estan vacios
+  while(izqMerge.length && derMerge.length) {
+    if(izqMerge[0] <= derMerge[0]) {
+      arrSort.push(izqMerge.shift())
+    } else {
+      arrSort.push(derMerge.shift())
+    }
+  }
+
+  return [...arrSort, ...izqMerge, ...derMerge]
+}
+
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
